3bcf05032e8ba7f098df88a2a3ba414bee913a2c
sharipovvzoirbek <zabyjorda@gmail.com>
2023-11-23 13:56:29 +0500

    6th

---
+++ fizz.py
@@ -0,0 +1,19 @@
+def friends(our):
+
+
+    print(our)
+
+friends("Hawk and Miguel")
+
+
+def school(e):
+
+    print(e)
+
+school("27-Maktab")
+
+
+def another(its):
+    print(its)
+another("no idea")
+

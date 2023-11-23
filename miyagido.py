5ad0bded4eed8ac9b2937e54254122d9ccb4c6a0
sharipovvzoirbek <zabyjorda@gmail.com>
2023-11-23 13:56:59 +0500

    7th

---
+++ miyagido.py
@@ -0,0 +1,21 @@
+def books(a,b):
+
+    print(a,b)
+
+
+books("Roberto Kiasaki","Alchemist")
+
+
+def noutbooks(a,b,c):
+
+    print(a,b,c)
+
+noutbooks("Lenovo","macbook","Asus")
+
+
+def planets(a,b,c):
+
+
+    print(a,b,c)
+
+planets("Vinera","Mars","earth")

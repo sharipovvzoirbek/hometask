36d053c2d091dbd7ee8e0f2d5bbba28654a0f868
sharipovvzoirbek <zabyjorda@gmail.com>
2023-11-23 13:55:29 +0500

    4th

---
+++ dojo.py
@@ -0,0 +1,21 @@
+def karate(dojo):
+
+    print(dojo)
+
+
+karate("Miyagido")
+
+
+def takewondo(dojo):
+
+    print(dojo)
+
+
+takewondo("Okinawa")
+
+
+def dojo(ck):
+
+    print(ck)
+
+dojo("Cobra Kai")

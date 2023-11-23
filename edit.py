4544e4364934da813a0fd4663735cfdf7ce389e3
sharipovvzoirbek <zabyjorda@gmail.com>
2023-11-23 13:56:02 +0500

    5th

---
+++ edit.py
@@ -0,0 +1,17 @@
+def main(text):
+
+    print(text)
+
+main("Cobra Kai")
+
+def info(key):
+
+    print(key)
+
+info("The best karate dojo")
+
+def about(text2):
+
+    print(text2)
+
+about("sensei is Johny Lowrans")

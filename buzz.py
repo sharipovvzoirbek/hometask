f00dd6132cf820945044caaa6c5b72a84bce6d7c
sharipovvzoirbek <zabyjorda@gmail.com>
2023-11-23 13:53:35 +0500

    first

---
+++ buzz.py
@@ -0,0 +1,18 @@
+def comp(len):
+
+    print(len)
+
+comp("kep the past in the past")
+
+
+def key(w):
+
+    print(w)
+
+key("no one is 4ever")
+
+def my(me="sharipov"):
+
+    print(me)
+
+my("Zoirbek")

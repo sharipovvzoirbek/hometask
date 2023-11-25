cb846216926c9a119fe3f6647eca8da197804a0e
sharipovvzoirbek <zabyjorda@gmail.com>
2023-11-25 21:14:15 +0500

    main

--- main.py
+++ main.py
@@ -1,3 +1,94 @@
 from deem import info
-a = info("text")
+from buzz import comp
+from buzz import key
+from buzz import my
+from dedd import us
+from dedd import math 
+from dedd import alg
+from deem import info
+from deem import fun 
+from deem import fav
+from dojo import karate 
+from dojo import takewondo
+from dojo import dojo 
+from edit import main 
+from edit import info  
+from edit import about 
+from fizz import friends 
+from fizz import school 
+from fizz import another 
+from miyagido import books 
+from miyagido import noutbooks
+from miyagido import planets
+from task import main 
+from task import name 
+from task import age 
+
+
+a = info()
 print(a)
+
+b = comp()
+print(b)
+
+c = key()
+print(c)
+
+d = my()
+print(d)
+
+e = us()
+print(e)
+
+f = math()
+print(f)
+
+g = alg()
+print(g)
+
+h = info()
+print(h)
+
+i = fav()
+print(i)
+
+j = fav()
+print(j)
+
+k = karate()
+print(karate)
+
+l = fun()
+print(l)
+
+m = takewondo()
+print(m)
+
+n = dojo()
+print(n)
+
+
+p = about()
+print(p)
+
+q = friends()
+print(q)
+
+r = school()
+print(r)
+
+s = another()
+print(s)
+
+t = books()
+print(t)
+
+u = noutbooks()
+print(u)
+
+v = planets()
+print(v)
+
+
+
+

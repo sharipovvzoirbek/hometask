a879e4a49fbc8b056578ea8dd2f5c17dbef1d990
sharipovvzoirbek <zabyjorda@gmail.com>
2023-11-25 21:10:45 +0500

    3

--- deem.py
+++ deem.py
@@ -1,18 +1,8 @@
-def  info(text):
+def  info():
+    return 'good movie'
+   
+def fun():
+    return "character"
 
-    print(text)
-
-info("Uzbekistan")
-
-
-def fun(add):
-
-    print(add)
-
-fun("27-school")
-
-def fav(job):
-
-    print(job)
-
-fav(1998)
+def fav():
+    return "my favourite movie is Cobra Kai"
